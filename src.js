731015b7c4b0b1c69ccaf1277691866fb71ab8e6
alitrms <ali@trms10.br>
2024-11-26 14:32:20 -0800

    sobre 100%

---
+++ src.js
@@ -0,0 +1,62 @@
+window.sr=ScrollReveal();
+
+sr.reveal('.about-nome',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 300,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+})
+sr.reveal('.title',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 400,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+})
+sr.reveal('.subtitulo',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 500,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+})
+sr.reveal('.about-paragrafo',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 600,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+})
+sr.reveal('.btn',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 700,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+})
+
+sr.reveal('.img-1',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1300,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+})
+
+sr.reveal('.img-2',{
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1500,
+    easing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+})
+
+
